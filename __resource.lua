6333c5d4bc77b5b804a28b94c178112f55ba766e
Aidan Ward <happychap612@gmail.com>
2022-09-16 05:41:28 +0100

    initial commit

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script "client.lua"
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+files {
+  "stream/int3232302352.gfx"
+}
